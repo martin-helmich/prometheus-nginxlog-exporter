51b15739b4bde9293ca8d167297017a088336ebc
Martin Helmich <m.helmich@mittwald.de>
2016-10-31 19:14:09 +0100

    Add missing comments

--- config/loader.go
+++ config/loader.go
@@ -3,10 +3,13 @@ package config
 import (
 	"fmt"
 	"io/ioutil"
+
+	"github.com/hashicorp/hcl"
 )
 
-import "github.com/hashicorp/hcl"
-
+// LoadConfigFromFile fills a configuration object (passed as parameter) with
+// values read from a configuration file (pass as parameter by filename). The
+// configuration file needs to be in HCL format.
 func LoadConfigFromFile(config *Config, filename string) error {
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -25,6 +28,8 @@ func LoadConfigFromFile(config *Config, filename string) error {
 	return nil
 }
 
+// LoadConfigFromFlags fills a configuration object (passed as parameter) with
+// values from command-line flags.
 func LoadConfigFromFlags(config *Config, flags *StartupFlags) error {
 	config.Listen = ListenConfig{
 		Port:    flags.ListenPort,
