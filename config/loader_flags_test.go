3aa966bbed572a008aa30510edecfc20c88ea8ea
Martin Helmich <kontakt@martin-helmich.de>
2018-01-05 17:28:17 +0100

    Add tests for flag parsing

---
+++ config/loader_flags_test.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"testing"
+	"reflect"
+)
+
+func configFromFlags(t *testing.T, flags StartupFlags) Config {
+	cfg := Config{}
+
+	err := LoadConfigFromFlags(&cfg, &flags)
+	if err != nil {
+		t.Error("unexpected error", err)
+	}
+
+	return cfg
+}
+
+func TestConfigContainsPortFromFlags(t *testing.T) {
+	cfg := configFromFlags(t, StartupFlags{
+		ListenPort: 1234,
+	})
+
+	if cfg.Listen.Port != 1234 {
+		t.Error("unexpected listen port", "expected", 1234, "actual", cfg.Listen.Port)
+	}
+}
+
+func TestConfigContainsFilenamesFromFlags(t *testing.T) {
+	sf := []string{"/foo.log", "/bar.log"}
+	cfg := configFromFlags(t, StartupFlags{
+		ListenPort: 1234,
+		Filenames: sf,
+	})
+
+	if len(cfg.Namespaces) != 1 {
+		t.Error("unexpected namespace count", "expected", 1, "actual", len(cfg.Namespaces))
+	}
+
+	if !reflect.DeepEqual(cfg.Namespaces[0].SourceFiles, sf) {
+		t.Error("unexpected source files", "expected", sf, "actual", cfg.Namespaces[0].SourceFiles)
+	}
+}
