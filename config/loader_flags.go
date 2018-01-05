a2e612e14c842beac26a26e240d7d3063345c592
Martin Helmich <kontakt@martin-helmich.de>
2018-01-05 17:15:08 +0100

    First simple refactoring

---
+++ config/loader_flags.go
@@ -0,0 +1,19 @@
+package config
+
+// LoadConfigFromFlags fills a configuration object (passed as parameter) with
+// values from command-line flags.
+func LoadConfigFromFlags(config *Config, flags *StartupFlags) error {
+	config.Listen = ListenConfig{
+		Port:    flags.ListenPort,
+		Address: "0.0.0.0",
+	}
+	config.Namespaces = []NamespaceConfig{
+		{
+			Format:      flags.Format,
+			SourceFiles: flags.Filenames,
+			Name:        flags.Namespace,
+		},
+	}
+
+	return nil
+}
