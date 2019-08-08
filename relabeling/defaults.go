791cc628950c6def883d9c4dc8b3e0ff11596103
SpontaneousOverthrow <v.rodionov@protonmail.com>
2019-08-08 21:28:45 +0300

    Fix syntax

--- relabeling/defaults.go
+++ relabeling/defaults.go
@@ -4,32 +4,31 @@ import "github.com/martin-helmich/prometheus-nginxlog-exporter/config"
 
 // DefaultRelabelings are hardcoded relabeling configs that are always there
 // and do not need to be explicitly configured
-
 var DefaultRelabelings = []*Relabeling{
-        {
-                config.RelabelConfig{
-                        TargetLabel: "method",
-                        SourceValue: "request",
-                        Split:       1,
+	{
+		config.RelabelConfig{
+			TargetLabel: "method",
+			SourceValue: "request",
+			Split:       1,
 
-                        WhitelistExists: true,
-                        WhitelistMap: map[string]interface{}{
-                                "CONNECT": nil,
-                                "DELETE":  nil,
-                                "GET":     nil,
-                                "HEAD":    nil,
-                                "OPTIONS": nil,
-                                "PATCH":   nil,
-                                "POST":    nil,
-                                "PUT":     nil,
-                                "TRACE":   nil,
-                        },
-                },
-        },
-        {
-                config.RelabelConfig{
-                        TargetLabel: "status",
-                        SourceValue: "status",
-                },
-        },
+			WhitelistExists: true,
+			WhitelistMap: map[string]interface{}{
+				"GET":     nil,
+				"HEAD":    nil,
+				"POST":    nil,
+				"PUT":     nil,
+				"DELETE":  nil,
+				"CONNECT": nil,
+				"OPTIONS": nil,
+				"TRACE":   nil,
+				"PATCH":   nil,
+			},
+		},
+	},
+	{
+		config.RelabelConfig{
+			TargetLabel: "status",
+			SourceValue: "status",
+		},
+	},
 }
