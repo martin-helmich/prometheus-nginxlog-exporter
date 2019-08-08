83e14604cd83b50b19a00cdef67dab9b1e80c66d
SpontaneousOverthrow <v.rodionov@protonmail.com>
2019-08-08 11:33:07 +0300

    Add WhitelistMap

--- relabeling/defaults.go
+++ relabeling/defaults.go
@@ -4,31 +4,43 @@ import "github.com/martin-helmich/prometheus-nginxlog-exporter/config"
 
 // DefaultRelabelings are hardcoded relabeling configs that are always there
 // and do not need to be explicitly configured
-var DefaultRelabelings = []*Relabeling{
-	{
-		config.RelabelConfig{
-			TargetLabel: "method",
-			SourceValue: "request",
-			Split:       1,
 
-			WhitelistExists: true,
-			Whitelist: []string{
-				"GET",
-				"HEAD",
-				"POST",
-				"PUT",
-				"DELETE",
-				"CONNECT",
-				"OPTIONS",
-				"TRACE",
-				"PATCH",
-			},
-		},
-	},
-	{
-		config.RelabelConfig{
-			TargetLabel: "status",
-			SourceValue: "status",
-		},
-	},
+func createMethodWhitelistMap() map[string]interface{} {
+        whitelistMap := make(map[string]interface{})
+        whitelist := []string{
+                "HEAD",
+                "POST",
+                "GET",
+                "PUT",
+                "DELETE",
+                "CONNECT",
+                "OPTIONS",
+                "TRACE",
+                "PATCH",
+        }
+
+        for i := range whitelist {
+                whitelistMap[whitelist[i]] = nil
+        }
+
+        return whitelistMap
+}
+
+var DefaultRelabelings = []*Relabeling{
+        {
+                config.RelabelConfig{
+                        TargetLabel: "method",
+                        SourceValue: "request",
+                        Split:       1,
+
+                        WhitelistExists: true,
+                        WhitelistMap:    createMethodWhitelistMap(),
+                },
+        },
+        {
+                config.RelabelConfig{
+                        TargetLabel: "status",
+                        SourceValue: "status",
+                },
+        },
 }
