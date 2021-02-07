996e1a44d746d6f8c098a4e828654f8d51d82c7b
yngwiewang <35023376@qq.com>
2021-02-07 22:31:41 +0800

    Add JSON log parser

---
+++ parser/jsonparser/jsonparser.go
@@ -0,0 +1,30 @@
+package jsonparser
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// JsonParser parse a JSON string.
+type JsonParser struct{}
+
+// NewJsonParser returns a new json parser.
+func NewJsonParser() *JsonParser {
+	return &JsonParser{}
+}
+
+// ParseString implements the Parser interface.
+// The value in the map is not necessarily a string, so it needs to be converted.
+func (j *JsonParser) ParseString(line string) (map[string]string, error) {
+	var parsed map[string]interface{}
+	err := json.Unmarshal([]byte(line), &parsed)
+	if err != nil {
+		return nil, fmt.Errorf("json log parsing err: %w", err)
+	}
+
+	fields := make(map[string]string, len(parsed))
+	for k, v := range parsed {
+		fields[k] = fmt.Sprintf("%v", v)
+	}
+	return fields, nil
+}
---
+++ parser/textparser/textparser_test.go
@@ -0,0 +1,43 @@
+package textparser
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestTextParse(t *testing.T) {
+	parser := NewTextParser(`[$time_local] $request_method "$request" $request_length $body_bytes_sent $status $request_time $upstream_response_time`)
+	line := `[03/Feb/2021:11:22:33 +0800] GET "GET /order/2145 HTTP/1.1" 123 518 200 0.544 0.543`
+	got, err := parser.ParseString(line)
+	if err != nil {
+		t.Error(err)
+	}
+	want := map[string]string{
+		"time_local":             "03/Feb/2021:11:22:33 +0800",
+		"request_time":           "0.544",
+		"request_length":         "123",
+		"upstream_response_time": "0.543",
+		"status":                 "200",
+		"body_bytes_sent":        "518",
+		"request":                "GET /order/2145 HTTP/1.1",
+		"request_method":         "GET",
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("TextParser.Parse() = %v, want %v", got, want)
+	}
+}
+
+func BenchmarkParseText(b *testing.B) {
+	parser := NewTextParser(`[$time_local] $request_method "$request" $request_length $body_bytes_sent $status $request_time $upstream_response_time`)
+	line := `[03/Feb/2021:11:22:33 +0800] GET "GET /order/2145 HTTP/1.1" 123 518 200 0.544 0.543`
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		res, err := parser.ParseString(line)
+		if err != nil {
+			b.Error(err)
+		}
+		_ = fmt.Sprintf("%v", res)
+	}
+}
