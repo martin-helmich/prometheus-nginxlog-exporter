4348122f4324e7d07db2001fba3a56a0c8bb34de
yngwiewang <35023376@qq.com>
2021-02-24 01:53:53 +0800

    1. Add Parser flag in LoadConfigFromFlags function. 2. Use testify package in parser/*._test file.

--- parser/jsonparser/jsonparser_test.go
+++ parser/jsonparser/jsonparser_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
-	
+
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,6 +14,7 @@ func TestJsonParse(t *testing.T) {
 
 	got, err := parser.ParseString(line)
 	require.NoError(t, err)
+
 	want := map[string]string{
 		"time_local":             "2021-02-03T11:22:33+08:00",
 		"request_time":           "0.544",
@@ -32,7 +33,7 @@ func TestJsonParse(t *testing.T) {
 func BenchmarkParseJson(b *testing.B) {
 	parser := NewJsonParser()
 	line := `{"time_local":"2021-02-03T11:22:33+08:00","request_length":123,"request_method":"GET","request":"GET /order/2145 HTTP/1.1","body_bytes_sent":518,"status": 200,"request_time":0.544,"upstream_response_time":"0.543"}`
-	b.StartTimer()
+
 	for i := 0; i < b.N; i++ {
 		res, err := parser.ParseString(line)
 		if err != nil {
